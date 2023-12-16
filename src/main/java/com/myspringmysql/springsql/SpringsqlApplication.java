1a0ce602e88dacf6035f202245d3b576602c4fa5
wizkhalifa64 <sona95986@gmail.com>
2023-12-16 22:42:32 +0530

    first commit

---
+++ src/main/java/com/myspringmysql/springsql/SpringsqlApplication.java
@@ -0,0 +1,12 @@
+package com.myspringmysql.springsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsqlApplication.class, args);
+    }
+}
---
+++ src/test/java/com/myspringmysql/springsql/SpringsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspringmysql.springsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
